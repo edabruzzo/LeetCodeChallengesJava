ed7787874dd823c5fe08d82e280254c7b6f9a8a9
Emmanuel Abruzzo <emmanuel.oliveira3@gmail.com>
2021-11-27 07:57:18 -0300

    Adiciona novas soluções para desafios da Aceleração GFT da Digital Innovation

--- src/main/java/br/com/abruzzo/turing/ExchangingCards.java
+++ src/main/java/br/com/abruzzo/turing/ExchangingCards.java
@@ -1,20 +1,17 @@
 package br.com.abruzzo.turing;
 import java.io.*;
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Iterator;
-import java.util.Set;
 
 
 public class ExchangingCards {
 
-    static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-    static PrintWriter out = new PrintWriter(System.out);
+    static BufferedReader in = null;
 
     private static final boolean DEBUG = true;
     private PrintWriter w;
     private StreamTokenizer token;
-    private HashSet<Integer> n1, n2;
+    private HashSet<Integer> entrada1, entrada2;
 
     private int a, b;
 
@@ -22,12 +19,12 @@ public class ExchangingCards {
 
         try {
             if (DEBUG) {
-                in = new BufferedReader(new InputStreamReader(new FileInputStream("/home/emmanuel/NetBeansProjects/Turing/src/main/java/br/com/abruzzo/turing/entradas.txt")));
+                in = new BufferedReader(new InputStreamReader(new FileInputStream("/home/emmanuel/NetBeansProjects/Turing/src/main/java/br/com/abruzzo/turing/entradas.txt.teste1")));
             } else {
                 in = new BufferedReader(new InputStreamReader(System.in));
             }
             token = new StreamTokenizer(in);
-            w = out;
+            w = new PrintWriter(System.out);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -37,14 +34,15 @@ public class ExchangingCards {
     public boolean entrada() {
         a = Integer.parseInt(proximo());
         b = Integer.parseInt(proximo());
-        if (a == 0 && b == 0) return false;
-        n1 = new HashSet<Integer>();
-        n2 = new HashSet<Integer>();
+        if (a == 0 && b == 0)
+            return false;
+        entrada1 = new HashSet<Integer>();
+        entrada2 = new HashSet<Integer>();
         for (int i = 0; i < a; i++) {
-            n1.add(Integer.parseInt(proximo()));
+            entrada1.add(Integer.parseInt(proximo()));
         }
         for (int i = 0; i < b; i++) {
-            n2.add(Integer.parseInt(proximo()));
+            entrada2.add(Integer.parseInt(proximo()));
         }
         return true;
     }
@@ -54,10 +52,12 @@ public class ExchangingCards {
 
         try {
             token.nextToken();
-            if (token.ttype == StreamTokenizer.TT_EOF) return null;
+            if (token.ttype == StreamTokenizer.TT_EOF)
+                return null;
             else if (token.ttype == StreamTokenizer.TT_NUMBER) {
                 return String.valueOf((int) token.nval);
-            } else return token.sval;
+            } else
+                return token.sval;
         } catch (Exception e) {
             e.printStackTrace();
             return null;
@@ -67,15 +67,17 @@ public class ExchangingCards {
 
     public void resolver() {
 
-        int s = n1.size();
-        Iterator<Integer> it = n2.iterator();
-        int cont = 0;
-        while (it.hasNext()) {
-            int num = it.next();
-            if (!n1.contains(num)) cont++;
-            else s--;
+        int s = entrada1.size();
+        Iterator<Integer> iterator = entrada2.iterator();
+        int contador = 0;
+        while (iterator.hasNext()) {
+            int numero = iterator.next();
+            if (!entrada1.contains(numero))
+                contador++;
+            else
+                s--;
         }
-        s = Math.min(s, cont);
+        s = Math.min(s, contador);
         w.println(s);
         w.flush();
     }
@@ -86,8 +88,6 @@ public class ExchangingCards {
     public static void main(String[] args) throws IOException {
         String l;
         int cards1, cards2;
-
-
         //Continue com o seu código aqui
         ExchangingCards solucao = new ExchangingCards();
         solucao.inicializacao();
